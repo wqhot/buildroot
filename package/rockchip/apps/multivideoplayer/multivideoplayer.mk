a303cd204b5c556eda985482ed8655e432c1bfa8
wqhot <wqhot@outlook.com>
2025-03-28 18:05:46 +0800

    feat(multivideoplayer): 更新 jssRecovery 并添加 jssServer
    - 更新 jssRecovery 脚本，修改启动和停止消息
    - 添加 jssServer 可执行文件和初始化脚本
    - 调整 jssRecoveryGUI 和 jssServer 的安装路径

--- package/rockchip/apps/multivideoplayer/multivideoplayer.mk
+++ package/rockchip/apps/multivideoplayer/multivideoplayer.mk
@@ -16,7 +16,8 @@ define MULTIVIDEOPLAYER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/applications $(TARGET_DIR)/usr/share/icons
 	mkdir -p $(TARGET_DIR)/etc/jssrecovery/templates
 	$(INSTALL) -D -m 0644 $(@D)/image/icon_jssrec.png $(TARGET_DIR)/usr/share/icons/
-	$(INSTALL) -D -m 0755 $(@D)/jssRecovery $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/jssRecoveryGUI/jssRecovery $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D -m 0755 $(@D)/jssServer/jssServer $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 0755 $(@D)/jssRecovery.desktop $(TARGET_DIR)/usr/share/applications/
 	$(INSTALL) -D -m 0644 $(@D)/2k.parts $(TARGET_DIR)/etc/jssrecovery/templates/
 	$(INSTALL) -D -m 0755 $(@D)/index.html $(TARGET_DIR)/var/www/index.html
@@ -30,6 +31,11 @@ endef
 define MULTIVIDEOPLAYER_INSTALL_INIT_SYSV
 $(INSTALL) -D -m 755 $(MULTIVIDEOPLAYER_PKGDIR)/S41watchdog \
 	$(TARGET_DIR)/etc/init.d/S41watchdog
+endef
+
+define MULTIVIDEOPLAYER_INSTALL_INIT_SYSV
+$(INSTALL) -D -m 755 $(MULTIVIDEOPLAYER_PKGDIR)/S51jssServer \
+	$(TARGET_DIR)/etc/init.d/S51jssServer
 endef
 
 $(eval $(qmake-package))
