b0a1ea96cd4da83243de5e3dd1240e968b22bad1
wqhot <wqhot@outlook.com>
2026-02-10 10:01:14 +0800

    feat: add video.parts

--- package/rockchip/apps/multivideoplayer/multivideoplayer.mk
+++ package/rockchip/apps/multivideoplayer/multivideoplayer.mk
@@ -20,6 +20,7 @@ define MULTIVIDEOPLAYER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/jssServer/jssServer $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 0755 $(@D)/jssRecovery.desktop $(TARGET_DIR)/usr/share/applications/
 	$(INSTALL) -D -m 0644 $(@D)/2k.parts $(TARGET_DIR)/Backup/templates/2k.parts
+	$(INSTALL) -D -m 0644 $(@D)/video.parts $(TARGET_DIR)/Backup/templates/video.parts
 	$(INSTALL) -D -m 0755 $(@D)/index.html $(TARGET_DIR)/var/www/index.html
 endef
 
